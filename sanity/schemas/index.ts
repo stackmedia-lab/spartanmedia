4d42b8b8e5306682aad0c073bcfcbf2d8d6c30ca
stackmedia-lab <rizwan.stackmedia@gmail.com>
2026-05-13 12:14:57 +0500

    Delete sanity/schemas directory

--- sanity/schemas/index.ts
+++
@@ -1,7 +0,0 @@
-import caseStudy from './caseStudy';
-import service from './service';
-import teamMember from './teamMember';
-import testimonial from './testimonial';
-import siteSettings from './siteSettings';
-
-export const schemaTypes = [caseStudy, service, teamMember, testimonial, siteSettings];
